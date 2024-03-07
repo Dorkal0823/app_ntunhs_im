3ec2f112e2a98dd22d58f847766d36d595a973c4
Dorkal0823 <trws0823@gmail.com>
2024-03-07 16:32:42 +0800

    2024-03-07 WEEK 3

--- AndroidStudioProjects/app/src/main/java/com/example/work0229/MainActivity.kt
+++ AndroidStudioProjects/app/src/main/java/com/example/work0229/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.work0229
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
 
 class MainActivity : AppCompatActivity() {
+    val TAG:String = MainActivity::class.java.simpleName
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler(Looper.getMainLooper())
 
         val TextView = findViewById<TextView>(R.id.TextView)
         val guess_button = findViewById<Button>(R.id.guess_button)
@@ -19,7 +25,14 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editText)
         var validate_num: Int
         var secret: Int = Random().nextInt(10) + 1
+        handler.postDelayed(
+            {Toast.makeText(this,"5秒後的操作執行了！",Toast.LENGTH_SHORT).show()
+            },5000)
 
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
+        }
 
         guess_button.setOnClickListener {
             TextView.text = editText.text
